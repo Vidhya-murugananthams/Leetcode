8fe08cbaedc933bcdeb59670a2880468d00ff4d1
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2024-09-03 11:41:40 +0530

    Time: 1 ms (85.16%), Space: 39.9 MB (94.92%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,55 +1,51 @@
 class Solution {
-    private int count = 0;  // Make count an instance variable
-
+private int count=0;
     public int totalNQueens(int n) {
-        if (n == 1) {
+        if(n==1)
             return 1;
-        } else if (n < 4) {
-            return 0;
-        }
+       else  if(n<4)
+        return 0;
         
-        int[][] arr = new int[n][n];
-        queen(arr, 0, n);
-        return count;
-    }
+        int arr[][]=new int [n][n];
+            queen(arr,0,0,n);
+    return count;}
     
-    private void queen(int[][] arr, int r, int n) {
-        if (r == n) {
+    private  void queen(int[][]arr, int r, int c, int n)
+    {
+        if(r==n)
+        {
             count++;
-            return;
+             return;
         }
-        
-        for (int c = 0; c < n; c++) {
-            if (ifsafe(arr, r, c, n)) {
-                arr[r][c] = 2;
-                queen(arr, r + 1, n);
-                arr[r][c] = 0;  // Backtrack
+        for(  c=0;c<n;c++)
+        {
+    if(ifsafe(arr, r,c,n)&& arr[r][c]==0){
+            arr[r][c]=2;
+      
+            queen(arr,r+1,c,n);
+            arr[r][c]=0;
             }
-        }
+        } }
+private  boolean ifsafe( int arr[][] , int r, int c, int n)
+{
+    for( int i=0;i<r;i++)
+    {
+        if(arr[i][c]==2)
+            return false;
+        
     }
+    for( int i=r-1, j=c-1 ; i>=0&&j>=0;i--,j--)
+    {
+        if(arr[i][j]==2)
+            return false;
+    }
+    for( int i=r-1, j=c+1 ; i>=0&&j<n;i--,j++)
+    {
+        if(arr[i][j]==2)
+            return false;
+    }
+    return true;
+}
+        
     
-    private boolean ifsafe(int[][] arr, int r, int c, int n) {
-        // Check column
-        for (int i = 0; i < r; i++) {
-            if (arr[i][c] == 2) {
-                return false;
-            }
-        }
-        
-        // Check left diagonal
-        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
-            if (arr[i][j] == 2) {
-                return false;
-            }
-        }
-        
-        // Check right diagonal
-        for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++) {
-            if (arr[i][j] == 2) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
 }
