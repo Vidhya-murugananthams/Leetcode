8280932cd649f16eb0b6a817e082c09c2825f1ee
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-01 19:13:56 +0530

    Time: 3 ms (74.15%), Space: 25.8 MB (6.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,20 +1,12 @@
 class Solution:
     def rotate(self, nums, k):
-        k %= len(nums)  # Handle cases where k is larger than the array size
-        
-        # Reverse the entire array
+        k %= len(nums)  
         self.reverse(nums, 0, len(nums) - 1)
-        # Reverse the first k elements
         self.reverse(nums, 0, k - 1)
-        # Reverse the remaining elements
         self.reverse(nums, k, len(nums) - 1)
     
     def reverse(self, nums, left, right):
-        """
-        Reverse elements in the array nums between indices left and right.
-        """
         while left < right:
-            # Swap the elements at left and right indices
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
