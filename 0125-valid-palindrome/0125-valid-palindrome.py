bffeedce2a0f682437c82d81141ac4cec58101be
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-02 07:33:41 +0530

    Time: 7 ms (81.21%), Space: 18.1 MB (38.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Clean the string by filtering out non-alphanumeric characters and converting to lowercase
-        cleaned_string = ''.join(c.lower() for c in s if c.isalnum())
-
-        # Use two pointers to check for palindrome
+        cleaned_string = ""
+        for char in s:
+            if char.isalnum(): 
+                cleaned_string += char.lower()  
+        
         l, h = 0, len(cleaned_string) - 1
         while l < h:
-            if cleaned_string[l] != cleaned_string[h]:
+            if cleaned_string[l] != cleaned_string[h]: 
                 return False
-            l += 1
-            h -= 1
-        return True
+            l += 1  
+            h -= 1 
+        
+        return True  
+
