379effa3f5914e12e8ab52e949acac66426a02a4
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-02 21:37:59 +0530

    Time: 33 ms (78.77%), Space: 40.2 MB (14.46%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -4,37 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        i = 1
-        fast = slow = head
-        prev_fast = None
-        prev_slow = None
-        while i < k:
-            prev_fast = fast
-            fast = fast.next
-            i += 1
-        begin_node = fast
-        while fast and fast.next:
-            prev_slow = slow
-            slow = slow.next
-            fast = fast.next
-        end_node = slow
-        if begin_node == end_node:
-            return head
-        if prev_slow:
-            prev_slow.next = begin_node
-        else:
-            head = fast
-
-        if prev_fast:
-            prev_fast.next = end_node
-        else:
-            head = slow
-
-        fast_next_node = begin_node.next
-        slow_next_node = end_node.next
-        begin_node.next = slow_next_node
-        end_node.next = fast_next_node
-        return head
-
-        
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+	    first = last = head
+	    for i in range(1, k):
+		    first = first.next
+		
+	    null_checker = first 
+	    while null_checker.next:
+		    last = last.next
+		    null_checker = null_checker.next
+	    first.val, last.val = last.val, first.val
+	    return head
