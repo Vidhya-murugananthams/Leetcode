4494b4297853d7b3220131b8f5e7a872870a456b
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-04 09:09:55 +0530

    Time: 5 ms (55.41%), Space: 18.9 MB (8.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,9 +1,10 @@
 class Solution:
-    def moveZeroes(self,nums:List[int])->None:
-        lastNonZeroIndex=0
+    def moveZeroes(self, nums: List[int]) -> None:
+        z=0
         for i in range(len(nums)):
             if nums[i]!=0:
-                nums[lastNonZeroIndex]=nums[i]
-                lastNonZeroIndex+=1
-        for i in range(lastNonZeroIndex,len(nums)):
-            nums[i]=0
+                nums[z],nums[i]=nums[i],nums[z]
+                z+=1
+      
+
+        
