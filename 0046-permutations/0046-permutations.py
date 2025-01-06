6fd751068e3bd3bdc3db3e610fae05e7e8fcc26c
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-06 21:25:59 +0530

    Time: 0 ms (100%), Space: 17.7 MB (29.15%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        self._permute_helper(nums, 0, len(nums), result)
+        result=[]
+        self.per(nums,0,len(nums),result) 
         return result
+    def per(self,nums,fi,n,result):
+        if fi==n-1:
+            result.append(nums[:])
+        for i in range(fi,n):
+            self.swap(fi,i,nums)
+            self.per(nums,fi+1,n,result) 
+            self.swap(fi,i,nums)
+    def swap(self,fi,i,nums):
+        nums[fi],nums[i]=nums[i],nums[fi]
 
-    def _permute_helper(self, nums, start, n, result):
-        if start == n - 1:
-            result.append(nums[:])  
-            return
-        
-        for i in range(start, n):
-            self._swap(nums, start, i)
-            self._permute_helper(nums, start + 1, n, result)
-            self._swap(nums, start, i)
 
-    def _swap(self, nums, i, j):
-        nums[i], nums[j] = nums[j], nums[i]
