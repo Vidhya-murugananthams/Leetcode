c69f0661ea9a05e67dd7e9f99be3920c02fbdf03
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-06 21:39:55 +0530

    Time: 0 ms (100%), Space: 18.2 MB (9.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,13 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        self._generate_combinations(n, 0, 0, "", result)
+    def generateParenthesis(self, n: int) -> List[str]:      
+        def _generate_combinations( n, open_count, close_count, current_str, result):
+            if close_count == n:
+                result.append(current_str)
+                return
+            if open_count < n:
+                _generate_combinations(n, open_count + 1, close_count, current_str + "(", result)
+            if open_count > close_count:
+                _generate_combinations(n, open_count, close_count + 1, current_str + ")", result)
+        result = []  
+        _generate_combinations(n, 0, 0, "", result) 
         return result
-
-    def _generate_combinations(self, n, open_count, close_count, current_str, result):
-        if close_count == n:
-            result.append(current_str)
-            return
-        if open_count < n:
-            self._generate_combinations(n, open_count + 1, close_count, current_str + "(", result)
-        if open_count > close_count:
-            self._generate_combinations(n, open_count, close_count + 1, current_str + ")", result)
