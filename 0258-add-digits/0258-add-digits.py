3293d6e4f269c27d1155ffb02d87e4a06f6df494
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-01-07 10:43:21 +0530

    Time: 5 ms (0.88%), Space: 18 MB (5.57%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,14 @@
 class Solution:
-    def addDigits(self, num: int) -> int:
-        a=list(map(int,str(num)))
-        if len(a)==1:
-            return a[0]
-
-        while len(a)>1:
-            cnt=sum(a)
-            a.clear()
-            a=list(map(int,str(cnt)))
-        return cnt
+    def addDigits(self, num: int) -> int:#56439
+        li=list(map(int,str(num)))# [5 6 4 3 9]
+        if len(li)==1:#5==1 f
+            return li[0]
+        while len(li)>1:# 5>1,2>1,1>1 f
+            ans=sum(li)#27, 9
+            li.clear()
+            li=list(map(int,str(ans)))#[2,7],[9]
+        return ans
+        
+        
+        
         
