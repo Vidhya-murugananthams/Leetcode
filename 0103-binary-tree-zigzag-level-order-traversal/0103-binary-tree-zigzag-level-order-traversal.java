7e9a038427575a6fcf23f2e155d113ae41cf17f6
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-20 11:02:25 +0530

    Time: 1 ms (69.7%), Space: 42.2 MB (76.57%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,37 +15,35 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> l=new ArrayList<>();
-        leftzigzag(root,1,l);
-        rightzigzag(root,1,l);
-        return l;
-    }
-    private void leftzigzag(TreeNode root,int level,List<List<Integer>> l){
+        List<List<Integer>> res=new ArrayList<>();
+        Queue<TreeNode> que=new LinkedList<>();
         if(root==null){
-            return;
+            return res;
         }
-        if(l.size()<level){
-            l.add(new ArrayList<>());
+        que.add(root);
+        boolean x=true;
+        while(!que.isEmpty()){
+            int s=que.size();
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i<s;i++){
+                TreeNode cur=que.poll();
+                if(x){
+                    temp.add(cur.val);
+                }else{
+                    temp.add(0,cur.val);
+                }
+                if(cur.left!=null){
+                    que.add(cur.left);
+                }
+                if(cur.right!=null){
+                    que.add(cur.right);
+                }  
+
+            }
+            res.add(temp);
+            x=!x;
+
         }
-        if(level%2==0){
-            l.get(level-1).add(root.val);
-        }
-        leftzigzag(root.right,level+1,l);
-        leftzigzag(root.left,level+1,l);
-        return;
-    }
-    private void rightzigzag(TreeNode root,int level,List<List<Integer>> l){
-        if(root==null){
-            return;
-        }
-        if(l.size()<level){
-            l.add(new ArrayList<>());
-        }
-        if(level%2!=0){
-            l.get(level-1).add(root.val);
-        }
-        rightzigzag(root.left,level+1,l);
-        rightzigzag(root.right,level+1,l);
-        return;
+        return res;
     }
 }
