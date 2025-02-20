59a312f156457e1782285343ff8f44c16d3bec12
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-20 22:27:18 +0530

    Time: 1 ms (69.7%), Space: 42 MB (94.81%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,49 +1,44 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        Queue<TreeNode> que=new LinkedList<>();
-        if(root==null){
-            return res;
-        }
-        que.add(root);
-        boolean x=true;
-        while(!que.isEmpty()){
-            int s=que.size();
-            List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<s;i++){
-                TreeNode cur=que.poll();
-                if(x){
-                    temp.add(cur.val);
-                }else{
-                    temp.add(0,cur.val);
+        if (root == null) return new ArrayList<>();
+        
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        boolean reverse = true;
+        
+        while (!q.isEmpty()) {
+            Deque<TreeNode> newQ = new ArrayDeque<>();
+            List<Integer> level = new ArrayList<>();
+            
+            for (TreeNode node : q) {
+                level.add(node.val);
+                if (reverse) {
+                    if (node.left != null) newQ.addFirst(node.left);
+                    if (node.right != null) newQ.addFirst(node.right);
+                } else {
+                    if (node.right != null) newQ.addFirst(node.right);
+                    if (node.left != null) newQ.addFirst(node.left);
                 }
-                if(cur.left!=null){
-                    que.add(cur.left);
-                }
-                if(cur.right!=null){
-                    que.add(cur.right);
-                }  
-
             }
-            res.add(temp);
-            x=!x;
-
+            
+            res.add(level);
+            q = newQ;
+            reverse = !reverse;
         }
+        
         return res;
     }
 }
