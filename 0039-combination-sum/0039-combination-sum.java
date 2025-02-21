26453207be6956135c2e7b89ca7f3ee3d47892f6
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 23:30:48 +0530

    Time: 2 ms (83.31%), Space: 44.6 MB (80.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,13 +1,12 @@
 class Solution {
     private List<List<Integer>> result;
-    private List<Integer> temp;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         result=new ArrayList<List<Integer>>();
-        temp=new ArrayList<Integer>();
-        comb(candidates,target,0);
+     List<Integer> temp=new ArrayList<Integer>();
+        comb(candidates,target,0,temp);
         return result;
     }
-    public void comb(int []nums, int target, int index)
+    public void comb(int []nums, int target, int index,List<Integer>temp)
     {
         if(target==0)
         {
@@ -19,7 +18,7 @@ class Solution {
             if(nums[i]>target)
                 continue;
             temp.add(nums[i]);
-            comb(nums,target-nums[i],i);
+            comb(nums,target-nums[i],i,temp);
             temp.remove(temp.size()-1);
         }
     }
