a36a7258bd4fe1fe7148822936519b48552ae682
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 22:20:25 +0530

    Time: 1 ms (93.02%), Space: 44.4 MB (81.07%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,6 @@
 public class Solution {
     public void permuteRec(int[] nums, int begin, List<List<Integer>> result) {
-        if (begin == nums.length) {
+        if (begin == nums.length-1) {
             List<Integer> temp = new ArrayList<Integer>();
             for (int num : nums) temp.add(num);
             result.add(temp);
