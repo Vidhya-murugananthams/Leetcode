0fb38ada0a35f4acd72e288d013a66185f83530a
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 21:44:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,24 @@
 class Solution {
-    static String keypad[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-
-    public List<String> letterCombinations(String digits) {
-       
-        List<String> l = new ArrayList<String>();
-     if(digits.length()==0)
-     return l;
-        possible(digits, 0, "", l);
-        return l;
-
-    }
-
-    public void possible(String s, int j, String ans, List<String> l) {
-        if (j == s.length()) {
-            l.add(ans);
+    public void solve(String digits, StringBuilder output, Integer ind, List<String> mapping, List<String> ans){
+        if(ind>=digits.length()){
+            ans.add(output.toString());
             return;
         }
-        String key = keypad[s.charAt(j) - 48];
-        for (int i = 0; i < key.length(); i++) {
-            possible(s, j + 1, ans + key.charAt(i), l);
+        Integer number = digits.charAt(ind)-'0';
+        String value = mapping.get(number);
+
+        for(int i=0; i<value.length(); i++){
+            output.append(value.charAt(i));
+            solve(digits, output, ind+1, mapping, ans);
+            output.deleteCharAt(output.length()-1);
         }
     }
-
-    public void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        if (s.length() == 0 || s.equals(""))
-            System.out.print("[]");
-        else {
-            List<String> l = letterCombinations(s);
-            System.out.print(l);
-        }
-
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if(digits.length()==0)return ans;
+        StringBuilder output = new StringBuilder();
+        List<String> mapping = Arrays.asList("", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
+        solve(digits, output, 0, mapping, ans);
+        return ans;
     }
 }
