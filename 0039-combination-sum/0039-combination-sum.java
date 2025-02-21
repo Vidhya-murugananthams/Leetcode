eb35afcb9da81a49e5c8eb7f3f69b0aeebb34119
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 23:28:20 +0530

    Time: 1 ms (99.87%), Space: 44.9 MB (21.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,26 @@
-import java.util.*;
-
 class Solution {
     private List<List<Integer>> result;
-    private ArrayList<Integer> temp;
-
+    private List<Integer> temp;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        result = new ArrayList<>();
-        temp = new ArrayList<>();
-        backtrack(candidates, target, 0);
+        result=new ArrayList<List<Integer>>();
+        temp=new ArrayList<Integer>();
+        comb(candidates,target,0);
         return result;
     }
-
-    private void backtrack(int[] candidates, int target, int index) {
-        if (target == 0) {
-            result.add(new ArrayList<>(temp)); // Add a copy of temp
+    public void comb(int []nums, int target, int index)
+    {
+        if(target==0)
+        {
+            result.add(new ArrayList<Integer>(temp));
             return;
         }
-
-        for (int i = index; i < candidates.length; i++) {
-            if (candidates[i] > target) continue; // Prune unnecessary calls
-
-            temp.add(candidates[i]); // Use ArrayList instead of int[]
-            backtrack(candidates, target - candidates[i], i);
-            temp.remove(temp.size() - 1); // Restore state (backtrack)
+        for (int i=index;i<nums.length;i++)
+        {
+            if(nums[i]>target)
+                continue;
+            temp.add(nums[i]);
+            comb(nums,target-nums[i],i);
+            temp.remove(temp.size()-1);
         }
     }
 }
