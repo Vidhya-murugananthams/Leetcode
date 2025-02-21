54bbcaccb31a806e41f7622aa10a9e2d9e7ead9b
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 22:49:33 +0530

    Time: 2 ms (83.31%), Space: 44.8 MB (46.02%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,28 @@
 import java.util.*;
 
 class Solution {
+    private List<List<Integer>> result;
+    private LinkedList<Integer> temp;
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates); // Sorting helps in pruning (optional for LeetCode)
-        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        result = new ArrayList<>();
+        temp = new LinkedList<>();
+        backtrack(candidates, target, 0);
         return result;
     }
 
-    private void backtrack(int[] candidates, int target, int index, List<Integer> temp, List<List<Integer>> result) {
+    private void backtrack(int[] candidates, int target, int index) {
         if (target == 0) {
             result.add(new ArrayList<>(temp));
             return;
         }
 
         for (int i = index; i < candidates.length; i++) {
-            if (candidates[i] > target) break; // Prune unnecessary calls
+            if (candidates[i] > target) continue; // Prune unnecessary calls
 
             temp.add(candidates[i]);
-            backtrack(candidates, target - candidates[i], i, temp, result);
-            temp.remove(temp.size() - 1); // Backtrack
+            backtrack(candidates, target - candidates[i], i);
+            temp.removeLast(); // Faster than remove(size - 1)
         }
     }
 }
