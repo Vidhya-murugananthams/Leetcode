65b87efcc60723ad03742ba1a4f2bd86d2bbad9c
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 22:36:13 +0530

    Time: 1 ms (93.02%), Space: 44.2 MB (98.22%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,31 @@
-public class Solution {
-    public void permuteRec(int[] nums, int begin, List<List<Integer>> result) {
-        if (begin == nums.length) {
-            List<Integer> temp = new ArrayList<Integer>();
-            for (int num : nums) temp.add(num);
-            result.add(temp);
-            return;
-        }
-        for (int i = begin; i < nums.length; i++) {
-            // Swap
-            int temp = nums[begin];
-            nums[begin] = nums[i];
-            nums[i] = temp;
-            
-            permuteRec(nums, begin + 1, result);
-            
-            // Swap back
-            temp = nums[begin];
-            nums[begin] = nums[i];
-            nums[i] = temp;
-        }
-    }
-    
+class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        permuteRec(nums, 0, result);
-        return result;
+        List<List<Integer>> list=new ArrayList<>();
+        perm(nums,0,list);
+        return list;
     }
+    public void perm(int[]nums,int fi,List<List<Integer>> list)
+    {
+        if(fi==nums.length)
+        {
+            List<Integer> temp=new ArrayList<Integer>();
+            for (int i:nums)
+                temp.add(i);
+            list.add(temp);
+            return ;
+        }
+
+        for (int i=fi;i<nums.length;i++)
+        {
+            int t=nums[fi];
+            nums[fi]=nums[i];
+            nums[i]=t;
+            perm(nums,fi+1,list);
+             t=nums[fi];
+            nums[fi]=nums[i];
+            nums[i]=t;
+
+        }
+    }
+
 }
