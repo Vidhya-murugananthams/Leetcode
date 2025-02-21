a59bc3d952989eab0135ec2996856fcb27537a52
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 22:44:18 +0530

    Time: 2 ms (83.31%), Space: 44.6 MB (63.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,34 +1,24 @@
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-         TreeSet<Integer> arr=new TreeSet<Integer>();
-       	List<List<Integer>>t=new ArrayList<>();
-         	ArrayList<Integer>temp=new ArrayList<Integer>();
-            ArrayList<Integer>input=new ArrayList<Integer>();
-        for(int i=0;i<candidates.length;i++)
-	{
-	    arr.add(candidates[i]);
-	}
-    input.addAll(arr);
-    comb_sum(input,t,temp,target,0);
-    return t;
-      }
+import java.util.ArrayList;
+import java.util.List;
 
-      public static void comb_sum(ArrayList<Integer> arr,List<List<Integer>> t,ArrayList<Integer> temp, int sum, int index)
-  {
-    if(sum==0)
-    {
-    
-  t.add(new ArrayList<>(temp));
-        return ;
+class Solution {
+    public void backtrack(int[] candidates, int target, List<Integer> combination, List<List<Integer>> result, int start) {
+        if (target == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        if (target < 0) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            combination.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], combination, result, i);
+            combination.remove(combination.size() - 1);
+        }
     }
-    for(int i=index;i<arr.size();i++)
-    {
-      if(sum-arr.get(i)>=0)
-      {
-        temp.add(arr.get(i));
-        comb_sum(arr,t,temp,sum-arr.get(i),i);
-        temp.remove(temp.size() - 1);
-      }}
-  }
-    
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, new ArrayList<>(), result, 0);
+        return result;
+    }
 }
