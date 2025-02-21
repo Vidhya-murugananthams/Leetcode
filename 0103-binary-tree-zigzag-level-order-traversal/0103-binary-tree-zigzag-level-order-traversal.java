d69c214f85553e3477a79e45baf9134931cc23df
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-21 14:58:12 +0530

    Time: 0 ms (100%), Space: 42.5 MB (23.94%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,41 +1,44 @@
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left, right;
-    TreeNode(int val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if (root == null) return new ArrayList<>();
-        
-        List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
+     List<List<Integer>> result=new ArrayList<>();
+        if(root ==null)
+            return result;
+        boolean flag=false;
+        Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
-        boolean reverse = false;
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>(size);
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                level.add(node.val);
-
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> level=new ArrayList<>();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode cur=q.poll();
+                level.add(cur.val);
+                if(cur.left!=null)
+                    q.add(cur.left);
+                if (cur.right!=null)
+                    q.add(cur.right);
             }
-
-            if (reverse) Collections.reverse(level);
-            res.add(level);
-            reverse = !reverse;
+            if(flag)
+                Collections.reverse(level);
+            result.add(level);
+            flag=!flag;
         }
         
-        return res;
+    return result;
     }
 }
