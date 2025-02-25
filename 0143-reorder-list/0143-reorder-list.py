a2623580d10c9c2e16712741d99186acc7d37d4d
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2025-02-25 13:01:43 +0530

    Time: 0 ms (100%), Space: 23.4 MB (38.37%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,32 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self,head):
-        cur=head
-        pre=None
-        next=None
-        while cur:
-            next=cur.next
-            cur.next=pre
-            pre=cur
-            cur=next
-        return pre
     def reorderList(self, head: Optional[ListNode]) -> None:
         if not head or not head.next:
             return
-        slow,fast=head,head
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-        second=self.reverse(slow.next)
-        slow.next=None
-
-        first=head
-        while first and second:
-          t1,t2=first.next,second.next
-          first.next=second
-          second.next=t1
-          first,second=t1,t2      
-
         
+        # Find the middle node
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Reverse the second half
+        head2 = self.reverse(slow.next)
+        slow.next = None  # Split the list
+        
+        # Merge two halves
+        while head and head2:
+            tmp1, tmp2 = head.next, head2.next
+            head.next, head2.next = head2, tmp1
+            head, head2 = tmp1, tmp2
+    
+    def reverse(self, node: ListNode) -> ListNode:
+        prev, curr = None, node
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev, curr = curr, nxt
+        return prev
         
