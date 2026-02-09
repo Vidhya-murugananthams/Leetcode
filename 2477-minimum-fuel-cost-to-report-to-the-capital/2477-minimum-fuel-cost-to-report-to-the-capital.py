81b03355e3f013b2c4569b154a47841d36efc1b9
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-09 22:10:24 +0530

    Time: 223 ms (39.25%), Space: 115.6 MB (7.17%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,17 +1,27 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        ans = 0
         graph = defaultdict(list)
-        for x, y in roads:
-            graph[x].append(y)
-            graph[y].append(x)
-        self.ans = 0
-        
-        def dfs(i, prev, people = 1):
-            for x in graph[i]:
-                if x == prev: continue
-                people += dfs(x, i)
-            self.ans += (int(ceil(people / seats)) if i else 0)
+
+        for u, v in roads:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        def dfs(u, prev):
+            nonlocal ans
+            people = 1
+
+            for v in graph[u]:
+                if v == prev:
+                    continue
+                people += dfs(v, u)
+            if u > 0:
+                ans += int(math.ceil(people/seats))
             return people
         
-        dfs(0, 0)
-        return self.ans
+        dfs(0, -1)
+        return ans
+
+        
+        
+        
