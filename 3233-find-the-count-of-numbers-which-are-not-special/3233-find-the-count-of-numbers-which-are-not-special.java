f18f7f5f4eebee333b48dfa3e78fd0ed8fdd7454
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-09 16:57:20 +0530

    Time: 10 ms (95.24%), Space: 46 MB (55.95%) - LeetHub

--- 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
+++ 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
@@ -1,22 +1,34 @@
-class Solution
-{
-    public int nonSpecialCount(int l, int r) 
-    {
-        int n=(int)Math.sqrt(r);
-        boolean[] isprime=new boolean[n+1];
-        Arrays.fill(isprime,true);
-        int c=0;
-        for(int i=2;i<=n;i++)
-        {
-            if(isprime[i])  
-            {
-                if(i*i<=r && i*i>=l) c++;
-                for(int j=i*i;j<=n;j+=i)
-                {
-                    isprime[j]=false;
+class Solution {
+    public int nonSpecialCount(int l, int r) {
+        int total = r - l + 1;
+
+        int start = (int) Math.ceil(Math.sqrt(l));
+        int end = (int) Math.floor(Math.sqrt(r));
+
+        if (start > end) return total;
+
+        boolean[] isPrime = sieve(end);
+
+        int special = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime[i]) special++;
+        }
+
+        return total - special;
+    }
+
+    private boolean[] sieve(int n) {
+        boolean[] prime = new boolean[n + 1];
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) prime[i] = true;
+            for (int i = 2; i * i <= n; i++) {
+                if (prime[i]) {
+                    for (int j = i * i; j <= n; j += i) {
+                        prime[j] = false;
+                    }
                 }
             }
         }
-        return r-l+1-c;
-    }}
-    
+        return prime;
+    }
+}
