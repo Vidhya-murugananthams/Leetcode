54a1fc68f62aef1b8863e5757f21d892aacda7dd
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-15 15:31:29 +0530

    Time: 3 ms (1.81%), Space: 19.6 MB (37.07%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,36 +1,51 @@
-from collections import deque
-
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.q1=deque()
+        self.q2=deque()
+
 
     def push(self, x: int) -> None:
         self.q1.append(x)
 
+        
+
     def pop(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-
-        popped_val = self.q1.popleft()
-
-        # swap queues
-        self.q1, self.q2 = self.q2, self.q1
-
-        return popped_val
+        if len(self.q1)==0:
+            return -1
+        else:
+            while len(self.q1)>1:
+                self.q2.append(self.q1.popleft())
+            t= self.q1.popleft()
+            self.q1, self.q2=self.q2,self.q1
+            return t
+            
+        
 
     def top(self) -> int:
-        while len(self.q1) > 1:
+        if len(self.q1)==0:
+            return -1
+        else:
+            while len(self.q1)>1:
+                self.q2.append(self.q1.popleft())
+            t= self.q1[0]
             self.q2.append(self.q1.popleft())
+            self.q1, self.q2=self.q2,self.q1
+            return t
 
-        top_val = self.q1[0]        # peek
-        self.q2.append(self.q1.popleft())
-
-        # swap queues
-        self.q1, self.q2 = self.q2, self.q1
-
-        return top_val
+        
 
     def empty(self) -> bool:
-        return len(self.q1) == 0
+        if self.q1:
+            return False
+        else:
+            return True
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
