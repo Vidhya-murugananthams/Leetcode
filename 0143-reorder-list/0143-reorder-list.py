eae10cb6ca388beb9913ba202ba88ab0e67a5e66
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-15 10:25:38 +0530

    Time: 0 ms (100%), Space: 27.9 MB (50.64%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,30 +5,26 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head or not head.next:
-            return
-        
-        # Find the middle node
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        # Reverse the second half
-        head2 = self.reverse(slow.next)
-        slow.next = None  # Split the list
-        
-        # Merge two halves
-        while head and head2:
-            tmp1, tmp2 = head.next, head2.next
-            head.next, head2.next = head2, tmp1
-            head, head2 = tmp1, tmp2
-    
-    def reverse(self, node: ListNode) -> ListNode:
-        prev, curr = None, node
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev, curr = curr, nxt
-        return prev
-        
+        slow=head
+        fast=head
+        while fast.next and fast.next.next:
+            slow=slow.next
+            fast=fast.next.next
+        cur =slow.next
+        slow.next=None
+        p=None
+        n=None
+        while cur:
+            n=cur.next
+            cur.next=p
+            p=cur
+            cur=n
+        first=head
+        second=p
+        while second:
+            fn=first.next
+            sn=second.next
+            first.next=second
+            second.next=fn
+            first=fn
+            second=sn
