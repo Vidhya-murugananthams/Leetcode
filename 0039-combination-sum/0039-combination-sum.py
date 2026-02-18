4be92d4f1a50a34bf5da379de50492409c16d6b6
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-18 14:41:24 +0530

    Time: 3 ms (95.8%), Space: 19.5 MB (67.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,16 @@
 class Solution:
-    
-    def combinationSum(self, candidates, target):
-        self.result = []
-        self.temp = []
-        self.comb(candidates, target, 0)
-        return self.result
-    
-    def comb(self, nums, target, index):
-        if target == 0:
+    def slove(self,candidates, index, target):
+        if target==0:
             self.result.append(self.temp[:])
             return
-        
-        for i in range(index, len(nums)):
-            if nums[i] > target:
+        for i in range(index,len(candidates)):
+            if candidates[i]>target:
                 continue
-            
-            self.temp.append(nums[i])
-            self.comb(nums, target - nums[i], i)   # same i (can reuse element)
-            self.temp.pop()   # backtracking
+            self.temp.append(candidates[i])
+            self.slove(candidates,i,target-candidates[i])
+            self.temp.remove(candidates[i])
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.result=[]
+        self.temp=[]
+        self.slove(candidates, 0, target)  
+        return self.result      
