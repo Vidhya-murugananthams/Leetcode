260dfd10d32dedb52013aad5f3673e4fac6bfda5
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-18 13:18:57 +0530

    Time: 3 ms (33.38%), Space: 49.1 MB (19.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,27 +1,36 @@
+import java.util.*;
+
 class Solution {
-    private String[]phone={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
     public List<String> letterCombinations(String digits) {
-    List<String> result=new ArrayList<String>();
-    if(digits.isEmpty())
-    return result;
-    comb(0,digits,new StringBuilder(),result);
-    return result;
-    }
-    public void comb(int index, String digits, StringBuilder temp, List<String> result )
-    {
-        if(index==digits.length())
-        {
-            result.add(new String(temp));
-            return;
-        }
-        String cur=phone[digits.charAt(index)-'0'];
-        for ( char c:cur.toCharArray())
-        {
-            temp.append(c);
-            comb(index+1,digits,temp,result);
-            temp.deleteCharAt(temp.length()-1);
-        }
+
+        List<String> list = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0)
+            return list;
+
+        String[] s = {
+                "", "", "abc", "def", "ghi",
+                "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        combination(0, digits, "", list, s);
+
+        return list;
     }
 
-        
+    public static void combination(int i, String digits, String current,
+                                   List<String> list, String[] s) {
+
+        if (i == digits.length()) {
+            list.add(current);
+            return;
+        }
+
+        String t = s[digits.charAt(i) - '0'];
+
+        for (char c : t.toCharArray()) {
+            combination(i + 1, digits, current + c, list, s);
+        }
     }
+}
