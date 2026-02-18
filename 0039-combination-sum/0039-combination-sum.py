885685d4594a42e88e378a85677886c22c75d36c
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-18 14:20:36 +0530

    Time: 3 ms (95.8%), Space: 19.4 MB (67.08%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def combinationSum(self, candidates, target):
+        self.result = []
+        self.temp = []
+        self.comb(candidates, target, 0)
+        return self.result
+    
+    def comb(self, nums, target, index):
+        if target == 0:
+            self.result.append(self.temp.copy())
+            return
+        
+        for i in range(index, len(nums)):
+            if nums[i] > target:
+                continue
+            
+            self.temp.append(nums[i])
+            self.comb(nums, target - nums[i], i)   # same i (can reuse element)
+            self.temp.pop()   # backtracking
