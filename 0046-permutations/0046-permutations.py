2f2a1991309389840051348e626de1ed775ab4ea
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-02-25 20:27:40 +0530

    Time: 0 ms (100%), Space: 19.7 MB (28.73%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,16 @@
 class Solution:
-    def swap(self,i,j,nums):
-        t=nums[i]
-        nums[i]=nums[j]
-        nums[j]=t
-    def slove(self, nums,fix,res):
+    def find(self,nums,fix,res):
         if fix==len(nums)-1:
             res.append(nums[ : ])
             return
-        for i in range(fix,len(nums)):
-            self.swap(i,fix,nums)
-            self.slove(nums,fix+1,res)
-            self.swap(i,fix,nums)
+        for i in range(fix,len( nums)):
+            nums[i], nums[fix]=nums[fix],nums[i]
+            self.find(nums, fix+1,res)
+            nums[i], nums[fix]=nums[fix],nums[i]
+
     def permute(self, nums: List[int]) -> List[List[int]]:
         res=[]
-        self.slove(nums,0,res)
+        self.find(nums,0,res)
         return res
 
         
