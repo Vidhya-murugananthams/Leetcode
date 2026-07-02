fe76fbc75154af95f01d09f09c50aac938ce210d
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-07-02 14:44:08 +0530

    Time: 7 ms (98.2%), Space: 59.8 MB (98.73%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,31 +1,45 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>(); // which will be our answer
-        List<String> path = new ArrayList<>(); // as we are generating list everythime, so at the end this will be our list
-        helper(0, s, path, res); // calling to recursion function start from index 0 and string s
-        return res;
-    }
-    // Entire recursive function, that generates all the partition substring
-    public void helper(int index, String s, List<String> path, List<List<String>> res){
-        // Base Condition, which means when we have done partition at the end (n), then add it to our ultimate result
-        if(index == s.length()){
-            res.add(new ArrayList<>(path));
+    int n;
+    boolean[][] is_palindrome;
+    String[][] substrings;
+
+    List<List<String>> ans;
+
+    void FindSubstrings(int ind, ArrayList<String> list) {
+        if (ind == n) {
+            ans.add(new ArrayList<String>(list));
             return;
         }
-        // Let's talk about partition
-        for(int i = index; i < s.length(); i++){
-            if(isPalindrome(s, index, i)){ // what we are checking over here is, if we partition the string from index to i Example-(0, 0) is palindrome or not
-                path.add(s.substring(index, i + 1)); // take the substring and store it in our list & call the next substring from index + 1
-                helper(i + 1, s, path, res); // as we have done for (0, 0) then our next will be from (1)
-                path.remove(path.size() - 1); // please make sure you remove when you backtrack. 
-                // Why? Because let say i had partion y, so when i go back. I can't have yy
-            }
+
+        for (int i = ind + 1; i <= n; i++) {
+            if (!is_palindrome[ind][i]) continue;
+            list.add(substrings[ind][i]);
+            FindSubstrings(i, list);
+            list.remove(list.size() - 1);
         }
-    } 
-    
-    public boolean isPalindrome(String s, int start, int end){ // A simple palindromic function start from 0 go till end. And basically keep on checking till they don't cross. 
-        while(start <= end){
-            if(s.charAt(start++) != s.charAt(end--)) return false;
+    }
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        is_palindrome = new boolean[n + 1][n + 1];
+        substrings = new String[n + 1][n + 1];
+        for (int i = 0; i < n; i++) for (int j = i + 1; j <= n; j++) {
+            substrings[i][j] = s.substring(i, j);
+            is_palindrome[i][j] = IsPalindrome(substrings[i][j]);
+        }
+
+        ans = new ArrayList<List<String>>();
+        FindSubstrings(0, new ArrayList<String>());
+        return ans;
+    }
+
+    boolean IsPalindrome(String s) {
+        int lower = 0;
+        int higher = s.length() - 1;
+        while (lower < higher) {
+            if (s.charAt(lower) != s.charAt(higher)) return false;
+            lower++;
+            higher--;
         }
         return true;
     }
