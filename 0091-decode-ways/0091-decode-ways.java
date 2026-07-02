ee0ff3b99f90b87be04961a5ccbff2c2fab19766
Vidhya-murugananthams <vickyvidhya125@gmail.com>
2026-07-02 13:35:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (93.1%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,25 +1,41 @@
 class Solution {
+    private int find(String s,int[] dp,int i)
+    {
+    if(i == s.length())
+        return 1;
+
+    if(s.charAt(i) == '0')
+        return 0;
+
+    if(dp[i] != -1)
+        return dp[i];
+
+    int ways = 0;
+
+    ways += find(s,dp,i+1);
+
+    if(i < s.length()-1)
+    {
+        int num =
+            (s.charAt(i)-'0')*10 +
+            (s.charAt(i+1)-'0');
+
+        if(num >= 10 && num <= 26)
+            ways += find(s,dp,i+2);
+    }
+
+    return dp[i] = ways;
+}
+
+
     public int numDecodings(String s) {
-        if (s.charAt(0) == '0') {
-            return 0;
-        }
+       //int num = Integer.parseInt(s);
+       int[] dp = new int[s.length()];
+       Arrays.fill(dp,-1);
+       dp[s.length()-1] = 1;
+       int ans = find(s, dp,0);
 
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
+       return ans == -1 ? 0 : ans;
 
-        for (int i = 2; i <= n; i++) {
-            int one = Character.getNumericValue(s.charAt(i - 1));
-            int two = Integer.parseInt(s.substring(i - 2, i));
-
-            if (1 <= one && one <= 9) {
-                dp[i] += dp[i - 1];
-            }
-            if (10 <= two && two <= 26) {
-                dp[i] += dp[i - 2];
-            }
-        }
-
-        return dp[n];        
     }
 }
